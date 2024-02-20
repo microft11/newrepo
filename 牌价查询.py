0c42fabf06335fa293bc257a4822e11f65eb71ab
ls <3243106232@qq.com>
2024-02-20 17:10:55 +0800

    finish

--- 牌价查询.py
+++ 牌价查询.py
@@ -2,6 +2,24 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+def change_name(name0):
+    driver = webdriver.Chrome("./chromedriver/chromedriver.exe")
+
+    try:
+        driver.get('https://www.11meigui.com/tools/currency')
+
+        name1 = driver.find_element(By.XPATH, "/html/body/main/div/table/tbody/'name0'/td/table[1]/tbody/tr[3]/td[5]")
+
+        return name1
+
+
+    except Exception as e:
+        print(f'Error: {str(e)}')
+        return None
+
+    finally:
+        driver.quit()
+
 
 def get_selling_rate(date, currency_code):
     driver = webdriver.Chrome("./chromedriver/chromedriver.exe")
@@ -24,11 +42,11 @@ def get_selling_rate(date, currency_code):
         currency_select.send_keys(currency_code)
 
         # 点击查询按钮
-        query_button = driver.find_element(By.CSS_SELECTOR, '.search_btn')
+        query_button = driver.find_element(By.CSS_SELECTOR, '#.main .search_btn')
         query_button.click()
 
         # 获取现汇卖出价
-        selling_rate = driver.find_element(By.XPATH, '//table[@class="publish"]/tbody/tr[2]/td[5]').text
+        selling_rate = driver.find_element(By.XPATH, '/html/body/div/div[4]/table/tbody/tr[2]/td[5]').text
 
         with open('result.txt', 'w') as f:
             f.write(selling_rate)
@@ -41,13 +59,14 @@ def get_selling_rate(date, currency_code):
 
     finally:
         # 关闭浏览器
-        time.sleep(10)
+        # time.sleep(20)
         driver.quit()
 
 
 # 使用示例
 date = '20211231'
-currency_code = '美元'
-selling_rate = get_selling_rate(date, currency_code)
+currency_code = 'USD'
+name = change_name((currency_code))
+selling_rate = get_selling_rate(date, name)
 print(selling_rate)
 
